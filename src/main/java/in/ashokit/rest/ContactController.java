1c915254f824b210552462835db222d200329b52
Niketan <nickthombare@gmail.com>
2022-08-11 13:34:06 +0530

    SLA:1234- m1() & m2() method is modified

--- src/main/java/in/ashokit/rest/ContactController.java
+++ src/main/java/in/ashokit/rest/ContactController.java
@@ -45,12 +45,12 @@ public class ContactController {
 	
     public void m1()
     {
-    	System.out.println("m1() method is added");
+    	System.out.println("m1() method is added......!");
 
     }
  public void m2()
     {
-    	System.out.println("m2() method is added");
+    	System.out.println("m2() method is added.....!");
 
     }
 }
